ae2f51ab7a07dae39627d5c7742e82b6a4fc46cc
BarGoldman <Bar8770@gmail.com>
2023-04-30 15:00:55 +0300

    copy demo

--- Demo.cpp
+++ Demo.cpp
@@ -15,109 +15,23 @@ using namespace std;
 
 using namespace ariel;
 
-int main()
-{
-    Fraction a(5, 3), b(14, 21);
+
+int main() {
+    Fraction a(5,3), b(14,21);
     cout << "a: " << a << "b: " << b << endl;
-    cout << "a+b" << a + b << endl;
-    cout << "a-b" << a - b << endl;
-    cout << "a/b" << a / b << endl;
-    cout << "a*b" << a * b << endl;
-    cout << "2.3*b" << 2.3 * b << endl;
-    cout << "a+2.421" << a + 2.421 << endl;
-    Fraction c = a + b - 1;
+    cout << "a+b" << a+b << endl; 
+    cout << "a-b" << a-b << endl; 
+    cout << "a/b" << a/b << endl; 
+    cout << "a*b" << a*b << endl; 
+    cout << "2.3*b" << 2.3*b << endl; 
+    cout << "a+2.421" << a+2.421 << endl; 
+    Fraction c = a+b-1;
     cout << c++ << endl;
     cout << --c << endl;
 
     cout << "c >=b ? : " << (c >= b) << endl;
-    if (a > 1.1)
-        cout << " a is bigger than 1.1" << endl;
-    else
-        cout << " a is smaller than 1.1" << endl;
-
-    cout << "--------------------- bar -----------------" << endl;
-    Fraction bar{12345, 23456};
-    Fraction aa {34567, 45678};
-    cout << "aa * bb " << ( bar * aa ) << endl;
-
-    // cout << "aa + bb " << ( bar == aa ) << endl;
-    // Fraction aa {5, -7};
-    // Fraction bb{3, -14};
-    // Fraction cc{13, -14};
-    // Fraction h = aa + bb ;
-    // cout << "aa + bb " << ( h == cc ) << endl;
+    if (a > 1.1) cout << " a is bigger than 1.1" << endl;
+    else cout << " a is smaller than 1.1" << endl;
 
 
-    // Fraction aaa(2, 5);
-    // Fraction h = 0.2001 - aaa;
-    // cout << "h " << (h) << endl;
-    // cout << "f.getNumerator() == 2)  " << (h.getNumerator() == -1) << endl;     // 4.654
-    // cout << "(f.getDenominator() == 1)  " << (h.getDenominator() == 5) << endl; // 4.654
-
-    Fraction aaa(2, 3);
-    Fraction h = 5.321 - aaa;
-    cout << "h " << (h) << endl;
-    cout << "f.getNumerator() == 2)  " << (h.getNumerator() == 2327) << endl;     // 4.654
-    cout << "(f.getDenominator() == 1)  " << (h.getDenominator() == 500) << endl; // 4.654
-
-    // Fraction d(1, 5);
-    // float e = 0.4; // Will be converted to 2/5
-    // Fraction g = e + d;
-    // cout << "f " << (g) << endl;
-    // cout << "f.getNumerator() == 2)  " << (g.getNumerator() == 2) << endl;     // 4.654
-    // cout << "(f.getDenominator() == 1)  " << (g.getDenominator() == 5) << endl; // 4.654
-
-    // Fraction aa(1, 3);
-    // cout << "aa:  " << (aa + 4.321) << endl; // 4.654
-
-    // Fraction neg_frac1{-2, 7};
-    // cout << "neg_frac:  " << neg_frac1++ << endl;
-
-    // std::stringstream ss;
-    // Fraction frac2{30, -60};
-    // Fraction bare{-1, 2};
-    // ss << bare;
-    // cout << "ss " << ss.str() << endl;
-    // cout << "bare:  " << bare << endl;
-    // cout << "c >=b ? : " << (frac2 == bare) << endl;
-
-    // Fraction neg_frac{-7, 9};
-    // ss << neg_frac;
-    // cout << "ss " << ss.str() << endl;
-    // cout << "neg_frac:  " << neg_frac << endl;
-
-    // //CHECK_EQ(Fraction{1, 3} + 4.321, 4.654);
-
-    // Fraction s{-2, 5};
-    // Fraction h{3, -5};
-    // cout << "s  > h ? : " << (s > h) << endl; // true
-
-    // Fraction bar{2,5};
-    // cout << "bar" << bar+0.0 << endl;
-
-    // Fraction bar1{-8, 14};
-    // Fraction bar2{4, -7};
-    // Fraction bb = 1*bar1;
-    // cout << "bar1 " << bar1 << endl; // -4/7
-    // cout << "bar1 " << bb << endl; // -4/7
-    // cout << "bar2 " << (bar1 == bar2) << endl; //
-
-    // Fraction bar3{7,9};
-    // Fraction bar4{1,2};
-    // Fraction ans{7 ,18};
-    // Fraction bb1 = (bar3 * 0.5);
-    // cout << "bb1 " << bb1 << endl;
-    // cout << "my test " << (bb1 == ans) << endl;
-
-    // // add to test A
-    // // (Fraction{6, 10} / Fraction{0, 10000})
-    // // CHECK_EQ(1 * Fraction{-8, 14}, Fraction{4, -7});
-
-    // Fraction test1{14,21};
-    // Fraction test2{28,42};
-    // cout << "s  > h ? : " << (test1 == test2) << endl;
-
-    // Fraction test3{1,2};
-    // cout << "s  > h ? : " << (test3 == test2) << endl;
-    cout << "--------------------- bar -----------------" << endl;
 }
